4ba2931dbf01b49d5aa028dca19da4eb0d55a215
Sean Callan <seancallan@gmail.com>
2015-12-11 22:01:57 -0800

    Setup supervisor and cowboy

--- lib/hydra.ex
+++ lib/hydra.ex
@@ -1,2 +1,18 @@
 defmodule Hydra do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(__MODULE__, [], function: :run)
+    ]
+
+    opts = [strategy: :one_for_one, name: Hydra.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def run do
+    {:ok, _} = Plug.Adapters.Cowboy.http(Hydra.Router, [])
+  end
 end
