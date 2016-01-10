f8c32fcf7ed07d9c64051bc1e7dd641cc954cb8c
Sean Callan <seancallan@gmail.com>
2016-01-09 19:06:11 -0800

    Extract HTTP functions from pipeline

--- lib/hydra/request.ex
+++ lib/hydra/request.ex
@@ -1,8 +1,18 @@
 defmodule Hydra.Request do
   @moduledoc """
-  Request struct
+  HTTP Request struct and functionality
   """
 
   @derive [Poison.Encoder]
   defstruct [:url, :resp_filter]
+
+  def get(url), do: HTTPoison.get(url)
+  def get!(url) do
+    case get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> body
+      _ -> %{}
+    end
+  end
+
+  def decode!(json), do: json |> Poison.decode!
 end
